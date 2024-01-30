2179169280860f591e43d2050069d2229186aea9
Elise Erickson <elisece717@gmail.com>
2024-01-30 07:30:52 -0500

    satisfies tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 0
+    while i < 10:
+        print(10 - i)
+        i += 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return([x ** 2 for x in int_list])
+    
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
